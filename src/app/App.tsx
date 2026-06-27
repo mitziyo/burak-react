66200212b92cba2b7aa52c3bd86ea95d529210ad
khan <ziyodullokhan009@mail.ru>
2026-06-27 15:37:22 +0900

    fix: Organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,32 +3,21 @@ import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
 import { backgroundColor } from "@mui/system";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Router, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
+  const location = useLocation(); // uselocation react router domning hook i bzga object va path beradi
+  console.log("location:", location)
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductsPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-        </ul>
-      </nav>
-
+    <>
+    {location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar/> }
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -43,7 +32,8 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
-    </div>
+      <Footer/>
+    </>
   );
 }
 
