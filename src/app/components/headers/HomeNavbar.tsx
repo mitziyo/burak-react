fbb569a1ab96806b07a7be96a934eac9be5339a0
khan <ziyodullokhan009@mail.ru>
2026-06-30 14:23:00 +0900

    fix: modify homeNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -46,10 +46,7 @@ export function HomeNavbar() {
 
             {!authmember ? (
               <Box>
-                <Button
-                  variant="contained"
-                  className="login-button"
-                >
+                <Button variant="contained" className="login-button">
                   Login
                 </Button>
               </Box>
@@ -58,12 +55,29 @@ export function HomeNavbar() {
                 className="user-avatar"
                 src={"/icons/default-user.svg"}
                 aria-haspopup={"true"}
-                
               />
             )}
           </Stack>
         </Stack>
-        <Stack>DETAIL </Stack>
+        <Stack className={"header-frame"}>
+          <Stack className={"detail"}>
+            <Box className={"head-main-txt"}>
+              World's Most Delicious Cousine
+            </Box>
+            <Box className={"wel-txt"}>The Choice, not just a choice</Box>
+            <Box className={"service-txt"}>24 hours service</Box>
+            <Box className={"signup"}>
+              {!authmember ? (
+                <Button variant={"contained"} className={"signup-button"}>
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Box className={"logo-frame"}>
+            <div className={"logo-img"}></div>
+          </Box>
+        </Stack>
       </Container>
     </div>
   );
