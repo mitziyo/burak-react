53b285407929ef0ed3e9453f251caf3f59f5ab5f
khan <ziyodullokhan009@mail.ru>
2026-06-30 18:32:29 +0900

    fix: modiyfy statistics sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -4,7 +4,8 @@ import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
-import Events from "./Events";
+import Events from "./Events"; //ts-ignore
+import "../../../css/home.css"
 
 export default function HomePage() {
   return (
