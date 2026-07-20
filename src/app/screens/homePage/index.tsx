2083bb514d757cae7782923663f417341d3dcea5
khan <ziyodullokhan009@mail.ru>
2026-07-21 06:55:40 +0900

    feat: create config & env variables

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -29,52 +29,7 @@ export default function HomePage() {
   const { popularDishes } = useSelector(popularDishesRetriever);
   // 3. Selector : Sore => Data
 
-  useEffect(() => {
-    // 1. Backend server data request => Data
-    const result = [
-      {
-        _id: "6a3a2c8fde66d2df9f6ed567",
-        productStatus: "PROCESS",
-        productCollection: "DISH",
-        productName: "Kebab",
-        productPrice: 12,
-        productLeftCount: 122,
-        productSize: "LARGE",
-        productVolume: 1,
-        productDesc: "delicious kebab",
-        productImages: [
-          "uploads/products/38dc7114-35e3-4b31-9140-2da6dc02a83f.jpg",
-        ],
-        productViews: 0,
-        createdAt: "2026-06-23T06:49:51.402Z",
-        updatedAt: "2026-06-23T06:49:51.402Z",
-        __v: 0,
-      },
-      {
-        _id: "6a3a230e1db9491983c7c3ee",
-        productStatus: "PROCESS",
-        productCollection: "DISH",
-        productName: "lavash",
-        productPrice: 12,
-        productLeftCount: 233,
-        productSize: "NORMAL",
-        productVolume: 1,
-        productDesc: "mazza",
-        productImages: [
-          "uploads/products/023224d4-af00-4136-8754-39c2b8853302.jpg",
-          "uploads/products/2a3cb61b-e7ba-4893-b3c9-09d200c06382.jpg",
-        ],
-        productViews: 2,
-        createdAt: "2026-06-23T06:09:18.474Z",
-        updatedAt: "2026-07-09T10:10:08.120Z",
-        __v: 0,
-      },
-    ];
-
-    // 2. Slice: Data => Store   datani slice orqali Storega saqlaydi.
-    //@ts-ignore
-    setPopularDishes(result);
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className={"homepage"}>
